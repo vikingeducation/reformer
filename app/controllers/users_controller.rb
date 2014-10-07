350c9f08189e8d1f8c6bcfba905bd3a045a844f2
Michael Alexander <alexandermw@gmail.com>
2014-10-07 14:39:16 -0700

    logic to allow flat or nested params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ def new
 end
 
 def create
-  #@user = User.new(:email => params[:email], :username => params[:username], :password => params[:password])
   @user = User.new(whitelisted_user_params)
+
   if @user.save
     redirect_to new_user_path
   else
@@ -19,7 +19,11 @@ end
 private
 
 def whitelisted_user_params
-  params.require(:user).permit(:email, :username, :password)
+  if params[:user][:email] && params[:user][:password] && params[:user][:username]
+    params.require(:user).permit(:email, :username, :password)
+  else
+    :email => params[:email], :username => params[:username], :password => params[:password]
+  end
 end
 
 end
