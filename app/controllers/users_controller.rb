d14f121196747b3413b9ac0edbe248e47715e723
Michael Alexander <alexandermw@gmail.com>
2014-10-07 07:45:39 -0700

    pure html form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,25 @@
 class UsersController < ApplicationController
 
 def new
+  @user = User.new
 end
 
 def create
-  @user = User.new
-  @user.email = params[:email]
-  @user.username = params[:username]
-  @user.password = params[:password]
-
+  #@user = User.new(:email => params[:email], :username => params[:username], :password => params[:password])
+  @user = User.new(whitelisted_user_params)
   if @user.save
     redirect_to new_user_path
   else
+    flash[:notice] = "Oh no!!!!!"
     render :new
   end
 
+end
+
+private
+
+def whitelisted_user_params
+  params.require(:user).permit(:email, :username, :password)
 end
 
 end
