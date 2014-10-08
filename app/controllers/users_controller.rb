ab9fda991cb3f7236cd2f4d8e9282ece3dea3238
Tom McLaughlin <tim5046@gmail.com>
2014-10-08 19:04:58 -0400

    edit and update working. just need styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 
   def create
     @user = User.new(user_params)
@@ -10,6 +17,19 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
 
