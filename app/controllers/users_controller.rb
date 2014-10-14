33528f52c173b0dd3069cf7b65370160b2692b4d
Andur Carr <carr.andur@gmail.com>
2014-10-13 20:12:20 -0700

    Finish reformer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -7,9 +11,26 @@ class UsersController < ApplicationController
     @user = User.new(whitelisted_user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to users_path
     else
       render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(whitelisted_user_params)
+      redirect_to users_path
+    else
+      render :edit
     end
   end
 
