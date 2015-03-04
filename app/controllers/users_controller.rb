078e96a7ca5b077824429aa2e24b80461cd4c8a2
Dan Berger <dsberger@gmail.com>
2015-03-04 15:13:33 -0800

    sorted out user params problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(whitelisted_user_params)
+    @user.save
+  end
+
+  private
+
+  def whitelisted_user_params
+    if params[:user]
+      params.require(:user).permit(:email, :username, :password)
+    else
+      { email: params[:email],
+        username: params[:username],
+        password: params[:password] }
+    end
   end
 end
