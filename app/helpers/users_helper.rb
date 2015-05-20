0bb715f9263cb695fabcbd6e7d6fb96b84bc8e62
niconan <nick.hax@gmail.com>
2015-05-20 16:16:55 -0700

    add error messages

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,12 @@ module UsersHelper
         username: params[:username] }
     end
   end
+
+  def form_field_error_message(resource, field_sym)
+    unless resource.errors[field_sym].empty?
+      content_tag(:span, class: 'error-message') do
+        "#{field_sym.to_s.titleize} #{resource.errors[field_sym].first}"
+      end
+    end
+  end
 end
