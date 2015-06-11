506b47d38498d294706d88312a71f5baa68757b3
telwell <me@trevorelwell.com>
2015-06-10 23:26:11 -0400

    Part I completed

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,6 +4,19 @@ class UserController < ApplicationController
 	end
 
 	def create
+		@user = User.new
+		@user.username = params["user"][:username]
+		@user.email = params["user"][:email]
+		@user.password = params["user"][:password]
+		if @user.save
+			redirect_to '/'
+		else
+			render :new
+		end
+	end
 
+private 
+	def whitelisted_user_params 
+		params.require(:user).permit(:username, :email, :password)
 	end
 end
