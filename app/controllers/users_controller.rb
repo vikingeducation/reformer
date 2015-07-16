30317fc88fd96f94bd4e7cb017f28969c6076d97
Adam <adam.kinson@gmail.com>
2015-07-16 19:19:29 -0400

    build form in basic HTML

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(whitelisted_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+
+  end
+
+
+
+  private
+
+  def whitelisted_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
