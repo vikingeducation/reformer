53737d3674bfdcf507f6a4952e1b390f0a2eca5a
Adam <adam.kinson@gmail.com>
2015-07-16 22:23:47 -0400

    build edit form with error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,18 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+
   def show
     @user = User.find(params[:id])
   end
+
 
   def new
     @user = User.new
   end
 
+
   def create
-    #@user = User.new(whitelisted_params)
     @user = User.new(parse_params(params))
 
     if @user.save
@@ -24,18 +26,38 @@ class UsersController < ApplicationController
 
   end
 
+
   def destroy
     @user = User.find(params[:id]).destroy
     redirect_to users_path
   end
 
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(parse_params(params))
+      redirect_to users_path
+    else
+      render :edit
+    end
+
+  end
+
+
 
   private
+
 
   def whitelisted_params
     params.require(:user).permit(:username, :email, :password)
   end
+
 
   def parse_params(passed_params)
     if params[:user]
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'users#new'
 
-  resources :users, only: [:new, :create, :index, :show, :destroy]
+  resources :users
 
 
   # The priority is based upon order of creation: first created -> highest priority.
