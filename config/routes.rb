a6941dfb7db6bcddbbdd46963f0f0005814fe338
ayva <olga.ayva@gmail.com>
2015-07-25 14:35:29 -0700

    edit, update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "users#new"
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :index, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
