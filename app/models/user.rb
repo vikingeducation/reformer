ae943e6b2a6fdbfad9b88297f25357e8fe13bf8a
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-07-25 11:27:36 -0500

    created controller, started html form for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ActiveRecord::Base
+  validates :username
+            :presence => true,
+            :uniqueness => true,
+            :length =>{ :in => 6..16 }
+            :on => :create
+            :message => "Not a valid username."
+
+  validates :email
+            :presence => true,
+            :uniqueness => true,
+            :length =>{ :in => 5..40 },
+            :format => { :with => /@/ },
+            :on => :create
+            :message => "Not a valid email."
+
+  validates :password
+            :presence => true,
+            :length =>{ :in => 6..40 },
+            :on => :create
+            :message => "Not a valid password."
+
+
 end
