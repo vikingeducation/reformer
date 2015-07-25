ddc7b77280c98e895c5e0f1d8f34a09000185d33
Joseph Lozano <lozanojoseph@gmail.com>
2015-07-25 10:07:13 -0700

    form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,15 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(params_hash)
+    user.save!
+    redirect_to new_user_path
+  end
+
+
+  private
+
+  def params_hash
+    params.require(:user).permit(:email, :username, :password)
   end
 end
