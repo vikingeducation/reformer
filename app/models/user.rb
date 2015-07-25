ac290794cbffffd05f55aa5cc58eda9e1fa9d21c
David <davidmeza@outlook.com>
2015-07-25 17:28:01 -0400

    update a few things

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,17 @@ class User < ActiveRecord::Base
   validates :username,
             :presence => {:message => "User name can't be blank." },
             :uniqueness => {:message => "User name already exists."},
-            :length => { :in => 4..16 },
-            :on => :create
+            :length => { :in => 4..16 }
 
   validates :email,
             :presence => {:message => "Email can't be blank." },
             :uniqueness => {:message => "Email is already registered."},
             :length => { :in => 5..40 },
-            :format => { :with => /@/ },
-            :on => :create
+            :format => { :with => /@/ }
 
-  validates :password, 
+  validates :password,
             :presence => {:message => "Password can't be blank."},
-            :length => { :in => 6..40 },
-            :on => :create
+            :length => { :in => 6..40 }
 
 
 end
