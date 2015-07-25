2c940cbf8f443c606bfd401462ceebd41c2f9458
alokp <alokpra@gmail.com>
2015-07-25 10:14:35 -0700

    working on form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+def new
+	@user = User.new
+end
+
+def create
+	@user = User.new(whitelist_new_user_input)
+	if @user.save
+		redirect_to user_path
+	else
+		render :new
+	end
+end
+
+def show
+end
+
+private
+
+def whitelist_new_user_input
+	params.require(:user).permit(:name, :email, :password)
+end
+
 end
