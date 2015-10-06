99ab71b4e6202ef84f11d1a92adb10addde245ca
leedu708 <djl265@cornell.edu>
2015-10-06 17:38:36 -0400

    completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,43 @@ class UsersController < ApplicationController
 
   end
 
+  def index
+
+    @users = User.all
+
+  end
+
+  def show
+
+    @user = User.find(params[:id])
+
+  end
+
+  def destroy
+
+    @user = User.find(params[:id]).destroy
+    redirect_to users_path
+
+  end
+
+  def edit
+
+    @user = User.find(params[:id])
+
+  end
+
+  def update
+
+    @user = User.find(params[:id])
+
+    if @user.update(whitelisted_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+
+  end
+
   private
 
   def whitelisted_params
