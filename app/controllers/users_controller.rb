171481260775152578a5bcf96cbf451c3513eb9c
Sia <siakaramalegos@gmail.com>
2015-12-28 19:45:12 -0600

    Adds basic html form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_url, notice: "User successfully created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
