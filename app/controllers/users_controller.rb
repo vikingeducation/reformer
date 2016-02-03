e8287cae497d2a710e655fbda4c5721aaafda42b
Kelsey James <kelseybjames@gmail.com>
2016-02-03 10:20:08 -0800

    basic form done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,23 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
 
   def create
-    @user = User.new( params[:user][:username], params[:user][:email] )
+    @user = User.new(whitelisted_params)
+    if @user.save
+      flash[:success] = 'New user created'
+      redirect_to new_user_path
+    else
+      flash.now[:error] = 'User was not created'
+      render :new
+    end
   end
 
   private
-  def whitelisted_params
-    params.require( :user ).permit( :username, :email, :password )
-  end
 
+  def whitelisted_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
 end
