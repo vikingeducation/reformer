94826a4f0a2376fb80c635094aadfb08439b5d62
Andrew Baik <and.baik@gmail.com>
2016-02-03 11:58:42 -0500

    finished basic form for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
   def new
-    render text: "Hello World!"
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
   end
 end
