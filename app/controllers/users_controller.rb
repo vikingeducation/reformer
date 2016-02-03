f98f7da0574aba91b0de0e9340d4ce6a860f4d52
Sam O'Keefe <samok13@gmail.com>
2016-02-03 09:36:30 -0800

    basic html form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  
+  def new
+  end
+
+  def create 
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to :new
+    else
+      render :new
+    end
+
+  end
+
+
+
+end
