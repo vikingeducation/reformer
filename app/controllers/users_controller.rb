d16a64cefd95935b132554d3cedb0d7003d12001
Andrew Baik <and.baik@gmail.com>
2016-02-03 12:54:43 -0500

    finished form_for, working on other features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,23 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path
+      redirect_to user_path(@user)
     else
       render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
