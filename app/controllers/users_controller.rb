7a279da89e39ae9efaa18e98e99d4436c8bda628
Deepa Kackar <dkackar99@yahoo.com>
2016-02-03 10:20:38 -0800

    Edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
       redirect_to user_path(@user)
     else
       render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
     end
   end
 
