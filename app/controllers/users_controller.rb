9284c6085d138adcb84eb7c71a5ee2f1cc13c8db
Julia Herron <julia.herron@gmail.com>
2016-02-03 09:34:17 -0800

    form_tag version

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :nested_or_flat, only: [:create]
 
 
   def new
@@ -25,5 +26,22 @@ class UsersController < ApplicationController
   def whitelisted_user_params
     params.require(:user).permit(:email, :username, :password)
   end
+
+  def nested_or_flat
+    unless params[:user]
+      params[:user] = {email: params[:email], username: params[:username], password: params[:password]}
+    end
+  end
+
+
+#   flat
+#     Parameters: {"utf8"=>"✓", "authenticity_token"=>"ElYux4YpBrp0r50aTYDpEkCl+8m4oKftacEOur+pbTVfhwK03ALtn1ReTjQiWOzohxL5Z07lX1O2oDHCfwunBg==", "email"=>"e", "username"=>"e@email.com", "password"=>"[FILTERED]", "commit"=>"Submit"}
+# Completed 400 Bad Request in 1ms (ActiveRecord: 0.0ms)
+
+
+# nested
+
+# Parameters: {"utf8"=>"✓", "authenticity_token"=>"ElYux4YpBrp0r50aTYDpEkCl+8m4oKftacEOur+pbTVfhwK03ALtn1ReTjQiWOzohxL5Z07lX1O2oDHCfwunBg==", "user"=>{"email"=>"e", "username"=>"e", "password"=>"[FILTERED]"}}
+
 
 end
