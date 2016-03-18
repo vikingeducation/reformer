434b6a29990e2d0d02644355b720e4ba586e8aa3
Steven Chang <prime_pork@hotmail.com>
2016-03-18 22:28:22 +1000

    nested parameters in my basic form and show page displays the newly created users details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
 
   def create
+    @user = User.new(whitelisted_user_params)
+    if @user.save
+      redirect_to @user
+    else
+      @message = "fail, try again"
+      render :new
+    end
   end
 
   def new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def whitelisted_user_params
+    params.require(:user).permit(:email,:username,:password)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
+  resources :users, only: [:show, :new, :create]
 
 
   # The priority is based upon order of creation: first created -> highest priority.
