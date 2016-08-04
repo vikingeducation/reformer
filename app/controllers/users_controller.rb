fc017a3c70ba068b4a51c334740fb2fdcedc2a2a
Alex Lach <alexglach@gmail.com>
2016-08-04 19:42:40 -0400

    finished form tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(nested_params)
 
-    if @user.save
+    if @user.save!
       redirect_to new_user_path
     else
       render :new
@@ -19,5 +19,13 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
+  def nested_params
+    unless params.keys.include?("user")
+      params[:user] = {username: params["username"], email: params["email"], password: params["password"]}
+    end
+    user_params
+  end
+
 end
 
