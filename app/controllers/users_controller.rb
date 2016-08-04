d4e568a4cbaab1e38613f73db7780aaa310e2e64
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-08-04 16:53:17 -0700

    Finish forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,23 @@ class UsersController < ApplicationController
 
   end
 
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(choose_user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
   private
 
 
