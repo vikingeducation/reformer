5b987355f0ac14919a3ec9b8b39f0013d587768a
Adrian Mui <adrian.mui@gmail.com>
2016-08-04 17:45:24 -0400

    form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+
+    else
+      render :new
+    end
+
   end
 
   private
 
     def user_params
-      params.
+      params.require(:users).permit(:username, :email, :password)
     end
 end
