1cdfc52b3fd548afe227c1dcf3755eb8be51ddf9
jsteenb2 <jonathansteenbergen@gmail.com>
2016-08-04 15:50:18 -0700

    added form fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 
   def new
-    "Hello World"
+    @user = User.new
   end
 
   def create
-
+    @user = User.new
+    redirect_to new_user_path
   end
 
 end
