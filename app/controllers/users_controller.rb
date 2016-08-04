8d44dfc1883477c499e03ca8797765971b6b8de6
Mike Lee <asackofwheat@gmail.com>
2016-08-04 19:15:25 -0400

    working on error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,33 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    redirect_to new_user_path
+    @user = User.new(user_params)
+    if @user.save
+      flash.notice = "Form created."
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash.notice = "Form updated."
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:email,:password)
   end
 
 end
