1308f45afdc2d0fa0658ebea55913af06982c7e2
Dylan Lynch <lynchd2@tcnj.edu>
2016-08-04 18:08:45 -0400

    Finish form, start create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
 
   def new
+
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "The user was created! :)"
+      redirect_to new_user_path
+    else
+      flash.now[:error] = "Something went wrong :("
+      render :new
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
