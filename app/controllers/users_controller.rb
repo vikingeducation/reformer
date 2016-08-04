03a16c3cd6a106608358cf0f5c01202b04e37082
Morgan Martin <morganm5201@gmail.com>
2016-08-04 19:06:59 -0400

    finish forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    @user = user_params_or_nah
     if @user.save
       flash[:success] = "The user was created! :)"
       redirect_to new_user_path
@@ -16,9 +16,20 @@ class UsersController < ApplicationController
   end
 
 
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def user_params_or_nah
+    if !params[:user].nil?
+      User.new(user_params)
+    else
+      User.new(username: params[:username],
+               email: params[:email],
+               password: params[:password])
+    end
   end
 end
