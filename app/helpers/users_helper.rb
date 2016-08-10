decc84c29870db878e01e13ae103bf783bdfd954
David J Watts <davidjeremywatts@gmail.com>
2016-08-10 13:08:17 -0700

    finish

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def field_error_message(user, sym)
+    unless user.errors[sym].empty?
+      content_tag(:li, class: "error-message") do
+         sym.to_s.titleize + " " + user.errors[sym].first
+      end
+    end
+  end
+
 end
