7895ae2f9e18b7970ea02d9375cbc192db430588
Deepak <grc.deepak@gmail.com>
2016-09-08 22:37:13 -0700

    rugged form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(whitelisted_user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
 
+  private
+  def whitelisted_user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
