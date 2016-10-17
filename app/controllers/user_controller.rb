b8372edf29c6f69839430456d4abda40768f883e
Tamal Riedman <triedman99@gmail.com>
2016-10-16 17:32:41 -0700

    Part 1: Naked HTML Form I

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,22 @@
 class UserController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(whitelisted_user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def whitelisted_user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20161016231304) do
   create_table "users", force: :cascade do |t|
     t.string   "username",   null: false
     t.string   "email",      null: false
+    t.string   "password",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
