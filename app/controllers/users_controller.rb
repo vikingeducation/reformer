c09dd1fe457811c2f1c95b41198d3bb77c37d0d1
Joe Bernardi <joebernardi@hotmail.com>
2016-11-30 15:55:25 -0600

    update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,5 +34,20 @@ class UsersController < ApplicationController
 	end
 
 
+	def edit
+
+		@user = User.find(params[:id])
+
+	end
+
+	def update
+
+		@user = User.find(params[:id])
+
+		@user.update(user_params( params[:user]))
+
+		redirect_to action: 'index'
+
+	end
 
 end
