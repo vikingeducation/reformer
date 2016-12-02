1caa6e77c4cc123d7b8b4c7b0c85de4446043eef
John Sawyer <sawyer@sawyermerchant.com>
2016-12-01 20:06:05 -0500

    Edit is dependent on show what

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    # puts params
+    # @user = User.new
+    @user = User.find_by_id(params[:id])
+
+  end
+
+  def update
+    @user = User.find_by_id(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "User Updated!"
+      redirect_to new_user_path
+    else
+      flash.now[:error] = @user.errors.full_messages
+      render :new
+    end
+  end
+
   private
 
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'users#new'
 
-  resources :users, only: [:create, :new]
+  resources :users, only: [:create, :new, :edit, :show]
 end
