b98596d70e4058ec89f79c7cb057e614ff7b934f
Jessica G <gillan.jessica@gmail.com>
2016-12-01 17:37:54 -0700

    finish form, form tag, form for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,24 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    user = user.new(user_params)
-  end
-end
+    @user = User.new(user_params)
 
+    if @user.save
+      flash[:success] = "User Saved!"
+      redirect_to new_user_path
+    else
+      flash.now[:error] = @user.errors.full_messages
+      render :new
+    end
+  end
 
   private
 
     def user_params
-      params.require(:user).permit()
+      params.require(:user).permit(:username, :email, :password)
     end
+end
