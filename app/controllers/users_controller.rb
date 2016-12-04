34be579abcd33a910e87ff0c85008e657cf7bb56
Kenny Kottenstette <kennykotten@gmail.com>
2016-12-03 18:33:46 -0800

    begin editing functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    #
+  end
+
+  def update
+    #
+  end
+
   private
     def user_params
       params.require(:user).permit(:username, :password, :email)
