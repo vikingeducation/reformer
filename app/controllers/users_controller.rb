4167c37cf53123fc1c91cca4dcdeca5109e9fc73
tt <ttwang.303@gmail.com>
2017-01-08 15:53:34 -0500

    finish part 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+
   def new
-    @user = User.new(user_params)
+    @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(get_params)
     if @user.save
       redirect_to new_user_path
     else
@@ -16,6 +17,18 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:email, :username, :password)
+    end
+
+    def get_params
+      params[:user] ? user_params : nest_params
+    end
+
+    def nest_params
+      {
+        username: params[:username],
+        email: params[:email],
+        password: params[:password]
+      }
     end
 
 end
