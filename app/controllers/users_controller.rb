98e4f1f68d5e0d9ba8415bbd6ff1e8ea0b5f91b2
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-02-26 16:16:48 +0800

    Add error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,25 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def new
+    @user = User.new
 
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to action: 'new'
+      redirect_to users_path
+    else
+      render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
