62d12c5038059ba0d98c87fba69fc764b37a50d9
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-02-26 15:18:25 +0800

    Complete HTML form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   def new
+
   end
 
   def create
+    @user = User.new(whitelisted_user_params)
+    if @user.save
+      redirect_to action: 'new'
+    end
   end
+
+  private
+  def whitelisted_user_params
+    params.require(:user).permit(:email, :username, :password)
+  end
+
+
+
 end
