30daa85c13bfb6b1c76a0507545d276d2706b43d
Anthony Sin <anthony.t.sin@gmail.com>
2017-02-25 22:44:10 -0500

    basic form ,form tag, form for

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(whitelisted_user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render action: "new"
+    end    
+  end
+
+  private
+
+  def whitelisted_user_params
+    if params[:user]
+      params.require(:user).permit(:username, :email, :password)
+    else
+      params.permit(:username, :email, :password)
+    end
+  end
+end
