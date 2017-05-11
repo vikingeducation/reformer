feb3cceeed9ec7ca939a7f730ecf2de501155df2
efraimmgon <efraimmaugusto@gmail.com>
2017-05-11 08:50:40 -0400

    update user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -25,6 +25,15 @@ class UserController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
