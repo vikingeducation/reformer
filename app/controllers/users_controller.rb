eee6b0872a4930a97e4202a9d600b826a46eb335
Roy Chen <roychenlei@gmail.com>
2017-08-22 10:14:29 +0800

    finished form_for form, added show and index actions/views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,18 @@ class UsersController < ApplicationController
     end
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to @user
     else
       render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   private
