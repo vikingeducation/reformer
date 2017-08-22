3c1ffcc0ec78c53033b3dea4dffbc665a8a36049
Roy Chen <roychenlei@gmail.com>
2017-08-22 09:24:48 +0800

    finished form built with only HTML

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(whitelisted_user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def whitelisted_user_params
+    params.require(:user).permit(:username, :password, :email)
   end
 end
