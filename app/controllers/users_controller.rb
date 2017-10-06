897143ee394223cae445615398bf86611c59f74d
Sean Luckett <whitecrane007@gmail.com>
2017-10-06 13:46:09 -0600

    New, identical user form except using form_for
    Also fixes problems with rendering :new template with previous user
    input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+
     render :new, locals: {
-      email: user_params.fetch(:email),
-      username: user_params.fetch(:username)
-    }
-  rescue
-    render :new, locals: {
-      email: '',
-      username: ''
+      email: @user.email,
+      username: @user.username
     }
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       redirect_to new_user_path
     else
       render :new, locals: {
-        email: user_params[:email],
-        username: user_params[:username]
+        email: @user.email,
+        username: @user.username
       }
     end
   end
