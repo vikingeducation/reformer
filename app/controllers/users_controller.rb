50d68735d53ba8b91d57db74b99d4c92e2c15955
Sean Luckett <whitecrane007@gmail.com>
2017-10-06 10:57:07 -0600

    Adds naked html form way to create users
    When validations fail, renders /new with previously entered data (other
    than password, of course). If successful, renders clean new view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
   def new
+    render :new, locals: {
+      email: user_params.fetch(:email, ''),
+      username: user_params.fetch(:username, '')
+    }
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, locals: {
+        email: user_params[:email],
+        username: user_params[:username]
+      }
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :username, :password)
   end
 end
