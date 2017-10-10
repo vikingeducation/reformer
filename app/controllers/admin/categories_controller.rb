72af1615b184a521615e74586752b6b8580c9fe5
Sean Luckett <whitecrane007@gmail.com>
2017-10-10 11:27:18 -0600

    Adds admin view to show a category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -5,6 +5,10 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def show
+    @category = Category.find params[:id]
+  end
+
   def new
     @category = Category.new
   end
