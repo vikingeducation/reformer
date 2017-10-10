810d61ca7e558f14697004d725c7c45cfe4d43c7
Sean Luckett <whitecrane007@gmail.com>
2017-10-10 10:00:18 -0600

    Create Categories
    Ability to create categories with errors/success messages is there.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,29 @@
 class Admin::CategoriesController < ApplicationController
   layout 'admin_portal'
 
+  def index
+    @categories = Category.all
+  end
+
   def new
     @category = Category.new
+  end
+
+  def create
+    @category = Category.new(cat_params)
+
+    if @category.save
+      flash[:success] = "Created category #{@category.name}"
+      redirect_to admin_categories_path
+    else
+      flash[:danger] = "Couldn't create category."
+      render :new
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:category).permit :name
   end
 end
