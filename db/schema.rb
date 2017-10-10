0589f8f3ecb013af2aae2a5c630fa2f692f5aea8
Sean Luckett <whitecrane007@gmail.com>
2017-10-10 14:26:16 -0600

    Adds products to domain

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009205327) do
+ActiveRecord::Schema.define(version: 20171010202419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20171009205327) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.decimal "price", precision: 8, scale: 2, null: false
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
@@ -29,4 +38,5 @@ ActiveRecord::Schema.define(version: 20171009205327) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "products", "categories"
 end
---
+++ spec/factories/products.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :product do
+    name "MyString"
+    price "9.99"
+    category nil
+  end
+end
