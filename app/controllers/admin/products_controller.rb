9071f115d682c8fc3fdd076555794e17a5550969
Sean Luckett <whitecrane007@gmail.com>
2017-10-11 11:08:47 -0600

    Can create new products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,28 @@
 class Admin::ProductsController < ApplicationController
   layout 'admin_portal'
 
+  def index
+  end
+
   def new
     @product = Product.new
+  end
+
+  def create
+    @product = Product.new prod_params
+
+    if @product.save
+      flash[:success] = "Created product #{@product.name}"
+      redirect_to admin_products_path
+    else
+      flash[:danger] = "Couldn't create product"
+      render :new
+    end
+  end
+
+  private
+
+  def prod_params
+    params.require(:product).permit :name, :price, :category_id
   end
 end
