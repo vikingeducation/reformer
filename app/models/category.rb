0979a8ab6939410a51a51f136e2101f8d43088f3
Sean Luckett <whitecrane007@gmail.com>
2017-10-11 14:45:35 -0600

    Adds products list to category show views

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
   include IdAvailable
 
   validates :name, presence: true, length: { in: 4..16 }
+
+  def products
+    @products ||= Product.where category_id: id
+  end
 end
