41d50bf2390839e00f1dcd381946d9af8d76da26
Sean Luckett <whitecrane007@gmail.com>
2017-10-18 15:02:05 -0600

    Adds User#show
    Not all data is dynamic. Not all links work. Many stories to remedy
    these.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,4 +5,9 @@ class Admin::UsersController < ApplicationController
     users = User.all.map { |user| ::UserDecorator.new(user) }
     render :index, locals: { users: users }
   end
+
+  def show
+    user = User.find params[:id]
+    render :show, locals: { user: UserDecorator.new(user) }
+  end
 end
