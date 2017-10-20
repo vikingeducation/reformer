48346a21a545c282066bf7e298eb2675903ab6a7
Sean Luckett <whitecrane007@gmail.com>
2017-10-20 08:42:02 -0600

    Adds tests for address validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,7 @@ class Address < ApplicationRecord
 
   has_many :user_addresses
   has_many :users, through: :user_addresses
+
+  validates :street_1, presence: true,
+            length: { maximum: 64 }
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid without a city' do
+    bad_address = build :address, city: nil, state_id: 1
+    bad_address.save
+    expect(bad_address.errors[:city]).to include 'must exist'
+  end
+
+  it 'is invalid without a state' do
+    bad_address = build :address, state_id: nil
+    bad_address.save
+    expect(bad_address.errors[:state]).to include 'must exist'
+  end
+
+  it 'is invalid without a street address' do
+    bad_address = build :address, street_1: nil
+    bad_address.save
+    expect(bad_address.errors[:street_1]).to include "can't be blank"
+  end
+
+  it 'is invalid when street address is too long' do
+    bad_address = build :address, street_1: 'a' * 65
+    expect(bad_address).not_to be_valid
+  end
 end
