c209dd61d348f7ae5437f7ab6a481163029d3398
Sean Luckett <whitecrane007@gmail.com>
2017-10-24 14:24:42 -0600

    Adds billing and shipping addresses to User#show

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -6,4 +6,20 @@ class UserDecorator < SimpleDelegator
   def signup_date
     created_at.strftime "%Y-%m-%d"
   end
+
+  def show_billing_address
+    if billing_address.present?
+      AddressDecorator.new(billing_address).full_address
+    else
+      'None assigned.'
+    end
+  end
+
+  def show_shipping_address
+    if shipping_address.present?
+      AddressDecorator.new(shipping_address).full_address
+    else
+      'None assigned.'
+    end
+  end
 end
