b75ddb8ce6376c785b2abf0a7f527eea96a3f104
anne <richardson.ae@gmail.com>
2017-12-06 15:03:41 -0600

    Breaks forms out into partial and adds edit resources

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,30 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:create, :edit, :update]
+
   def new
     @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
   end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+
 end
